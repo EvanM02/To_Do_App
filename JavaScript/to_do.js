c9c08c8ab345169686ddb36280937a89f38eac4b
Epersonal <92110978+EvanM07@users.noreply.github.com>
2023-04-05 15:33:03 -0700

    Add form validation to prevent empty list items and add sound effects on item addition and removal.
    This commit includes several changes to the codebase. Firstly, I added form validation to prevent users from adding empty list items. I added sound effects to play when the user adds or removes an item from the list. I These changes should improve the user experience and make the application more robust.

--- JavaScript/to_do.js
+++ JavaScript/to_do.js
@@ -15,6 +15,37 @@ function add() {
   /* this takes the inputted task and creates a variable of it */
   var task = document.getElementById("task").value;
 
+  /* Sanitize user input to prevent XSS attacks */
+  task = DOMPurify.sanitize(task);
+
+  // Check if the input field is empty
+  if (task.trim() === "") {
+    // create a new Audio object and set the source of the sound file
+    var audio = new Audio("/Audio/error_sound.wav");
+    // play the sound effect
+    audio.play();
+    // Add animation to the error message
+    var newTask = document.querySelector("li:last-child");
+    newTask.classList.add("animation", "add-animation");
+
+    // Show a message to the user that the user needs to input something in the input field
+    var messagetwo = document.createElement("div");
+    messagetwo.textContent = "Please input a task !";
+    messagetwo.classList.add("messagetwo");
+    document.body.appendChild(messagetwo);
+
+    // Remove the message after 1 seconds
+    setTimeout(function () {
+      messagetwo.parentNode.removeChild(messagetwo);
+    }, 1000);
+
+    // Reload the page after 2 seconds
+    setTimeout(function () {
+      window.location.reload();
+    }, 1200);
+
+    return;
+  }
   var todos = get_todos();
   /* this adds a new task the end of the array*/
   todos.push(task);
@@ -22,7 +53,31 @@ function add() {
   localStorage.setItem("todo", JSON.stringify(todos));
   document.getElementById("task").value = "";
   show();
-  window.location.reload();
+
+  // Add animation to the new task
+  var newTask = document.querySelector("li:last-child");
+  newTask.classList.add("animation", "add-animation");
+
+  // Show a message to the user that the task was added
+  var message = document.createElement("div");
+  message.textContent = "Task added successfully";
+  message.classList.add("message");
+  document.body.appendChild(message);
+
+  // create a new Audio object and set the source of the sound file
+  var audio = new Audio("/Audio/add_sound.wav");
+  // play the sound effect
+  audio.play();
+
+  // Remove the message after 2 seconds
+  setTimeout(function () {
+    message.parentNode.removeChild(message);
+  }, 2000);
+
+  // Reload the page after 2 seconds
+  setTimeout(function () {
+    window.location.reload();
+  }, 2200);
 }
 
 /* this function keeps the tasks permanently displayed on the screen*/
@@ -36,8 +91,12 @@ function show() {
   for (var i = 0; i < todos.length; i++) {
     /* this also displays the tasks as a list and creates the button with a "x" */
     html +=
-      "<li>" + '<button class="remove" ' + i + "> x </button> " + todos[i];
-    ("</li>");
+      "<li>" +
+      '<button class="remove" id="' +
+      i +
+      '"> x </button> ' +
+      todos[i] +
+      "</li>";
   }
   html += "</ul>";
   /* this displays the task as a list */
@@ -62,7 +121,38 @@ function remove() {
   var todos = get_todos();
   todos.splice(id, 1);
   localStorage.setItem("todo", JSON.stringify(todos));
-  window.location.reload();
+
+  // Update the UI to remove the task
+  show();
+
+  // Add animation to the removed task
+  var removedTask = document.querySelector(
+    "li:nth-child(" + (parseInt(id) + 1) + ")"
+  );
+
+  // Add animation to the removed task
+  removedTask.classList.add("animation", "remove-animation");
+
+  // Show a message to the user that the task was removed
+  var message = document.createElement("div");
+  message.textContent = "Task removed successfully";
+  message.classList.add("message");
+  document.body.appendChild(message);
+
+  // create a new Audio object and set the source of the sound file
+  var audio = new Audio("/Audio/remove_sound.wav");
+  // play the sound effect
+  audio.play();
+
+  // Remove the message after 2 seconds
+  setTimeout(function () {
+    message.parentNode.removeChild(message);
+  }, 2000);
+
+  // Reload the page after 2 seconds
+  setTimeout(function () {
+    window.location.reload();
+  }, 2200);
 }
 /* this tells the browser how to display the todo array after an item as been removed */
 var buttons = document.getElementsByClassName("remove");
